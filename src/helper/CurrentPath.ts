44c4b20202876e918f031c1ed09e1575e8f9e0c7
UnDer-7 <mateus7532@gmail.com>
2023-12-27 15:47:32 -0300

    removing JS

--- src/helper/CurrentPath.ts
+++
@@ -1,4 +0,0 @@
-export function CurrentPath(): string {
-    // @ts-ignore: pkg is added by pkg lib
-    return (process.pkg) ? process.cwd() : __dirname;
-}
--- src/helper/Run.ts
+++
@@ -1,7 +0,0 @@
-export function Run(func: () => void): void {
-    func();
-}
-
-export async function RunAsync(func: () => Promise<void>): Promise<void> {
-    await func();
-}
